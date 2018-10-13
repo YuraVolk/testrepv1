44b54226b33253f978a849555ae1809ca595f661
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2018-10-13 10:13:30 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -8,7 +8,7 @@ sendraw,
                                                                                 
 ffmpeg -i input_video.mp4 -ss 00:00:30 -t 00:00:10 -c:a copy -c:v copy cropped.mp4        
                                                                                 
-convert -resize 50% -depth 8 -background transparent plate.svg plate.png        
+convert -resize 50% -depth 8 -background none plate.svg plate.png        
                                                                                 
 ffmpeg -i cropped.mp4 -i plate.png -filter_complex "[0:v][1:v] overlay=20:20:en$
                                                                                 
