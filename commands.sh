514e2f87a63dd4909c08aa6a5216f591b691b43b
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2018-10-13 11:43:45 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -12,7 +12,7 @@ convert -depth 8 -background none -resize 50% plate.svg plate.png
                                                                                 
 ffmpeg -i cropped.mp4 -i plate.png -filter_complex "[0:v][1:v] overlay=0:446:enable=between(t\,1\,9)" -c:a copy plate.mp4
 
-ffmpeg -i plated.mp4 -vf drawtext="fontfile=arial.ttf:text='Last Test':fontsize=20:fontcolor=black:x=200:y=476:overlay=2:9" -c:a copy out.mp4
+ffmpeg -i plated.mp4 -vf drawtext="fontfile=arial.ttf:text='Last Test':fontsize=20:fontcolor=black:x=200:y=476:enable='between(t\,2\,9)'" -c:a copy out.mp4
                                                                                 
 )                                                                               
             
