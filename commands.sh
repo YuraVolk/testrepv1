ffee34e6ae44b9cdca4c24201716d4d1bc68fcc3
YuraVolk <42916748+YuraVolk@users.noreply.github.com>
2018-10-13 11:53:42 +0300

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -10,7 +10,7 @@ ffmpeg -i input_video.mp4 -ss 00:00:30 -t 00:00:10 -c:a copy -c:v copy cropped.m
                                                                                 
 convert -depth 8 -background none -resize 50% plate.svg plate.png        
                                                                                 
-ffmpeg -i cropped.mp4 -i plate.png -filter_complex "[0:v][1:v] overlay=0:446:enable=between(t\,2\,9)" -c:a copy plate.mp4
+ffmpeg -i cropped.mp4 -i plate.png -filter_complex "[0:v][1:v] overlay=0:446:enable=between(t\,2\,9)" -c:a copy plated.mp4
 
 ffmpeg -i plated.mp4 -vf drawtext="fontfile=Arial.ttf:text='Last Test':fontsize=20:fontcolor=black:x=200:y=476:enable='between(t\,3\,9)'" -c:a copy out.mp4
                                                                                 
